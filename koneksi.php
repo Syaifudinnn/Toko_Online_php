9132d0736f122498f6f1699b95153423b2743ff8
Mukhammad Syaifudin Adn <99938083+Syaifudinnn@users.noreply.github.com>
2023-03-02 14:34:49 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'toko');
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+?>
